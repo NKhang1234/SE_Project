4e8f8f6ed1f4aae407ade220656303deab75da19
Qdaika <uchihajashua@gmail.com>
2024-12-10 10:58:36 +0700

    publisher

--- src/routes/publisherRouter.js
+++ src/routes/publisherRouter.js
@@ -1,22 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
-const publisherController = require('../app/controllers/publisherController.js');
+const publisherController = require("../app/controllers/publisherController.js");
 
+const upload = multer({ dest: "src/resources/img" });
+
+// Test upload image
+router.post("/upload", upload.single("image"), publisherController.upload);
 // Xem danh sach cac offer
-router.get('/offerStatus', publisherController.offerStatus);
+router.get("/offerStatus", publisherController.offerStatus);
 router.get(
-    '/offerStatus/:type/:content',
-    publisherController.offerStatusFilter
+  "/offerStatus/:type/:content",
+  publisherController.offerStatusFilter
 );
 
 // them mot offer
-router.post('/add', publisherController.add);
+router.post("/add", upload.single("book_img"), publisherController.add);
+
 // neu co add page
-router.get('/add', publisherController.getAddPage);
+router.get("/add", publisherController.getAddPage);
 
 // sua mot offer
-router.get('/edit/:id', publisherController.editPage);
-router.put('/edit', publisherController.edit);
+router.get("/edit/:id", publisherController.editPage);
+router.post("/edit", upload.single("book_img"), publisherController.edit);
 
 module.exports = router;
