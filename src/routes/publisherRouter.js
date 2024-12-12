81ecbdaae5002556e434653da5ca0fc603b92dfa
mahara0511 <122422522+mahara0511@users.noreply.github.com>
2024-12-12 16:36:26 +0700

    publisher router

--- src/routes/publisherRouter.js
+++ src/routes/publisherRouter.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const multer = require('multer');
 
 const publisherController = require('../app/controllers/publisherController.js');
-
+const site = require('../app/controllers/siteController.js');
 const upload = multer({ dest: 'src/resources/img' });
 
+router.use(site.publisherAuthenticated);
 // Test upload image
 router.post('/upload', upload.single('image'), publisherController.upload);
 // Xem danh sach cac offer
